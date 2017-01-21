4f15e76ba526f0c3aac515eb835616a3ef9ffea4
craig91 <craigdejean1>
2017-01-20 22:07:50 -0500

    added variable basics

---
+++ all-about-variables/making-variables.js
@@ -0,0 +1,31 @@
+// A variable is a name associated with a value.
+// Variables allow you to store and manipulate data in your programs. 
+
+
+i = 2;
+console.log(i);
+// the number 2 is assined to i, so if you console log i you would get 2, not i.
+
+// most of the time, you would want to declare a variable using the "var" keyword.
+// I'll explain why in a future tutorial.
+
+var i = 2;
+
+// one important thing to note that javascript is an "untyped" language, so variables are able to 
+// hold any datatypes, unlike Java or C.
+
+var i = 2;
+var i = "tow"
+var k = function logToTen() {
+    for(var i = 0; i < 10; i++){
+        console.log(i)
+    } 
+}
+
+var i = [1,2,3,4,5,6,7,8,9,10];
+var i = {
+    title: "This is an object",
+    author: "Craig Dejean"
+};
+
+// these are several examples that demonstrate that variables can hold different datatypes.
