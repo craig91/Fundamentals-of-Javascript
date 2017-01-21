1659f7efc041de2f807d2d63182068aba2787e5a
craig91 <craigdejean1>
2017-01-20 22:19:09 -0500

    added more info in variables

--- all-about-variables/making-variables.js
+++ all-about-variables/making-variables.js
@@ -29,3 +29,26 @@ var i = {
 };
 
 // these are several examples that demonstrate that variables can hold different datatypes.
+
+// There are several ways you can declare variables:
+
+a = 1;
+var a = 1;
+
+var sum, product;
+// makes two variables in one declaration
+
+var sum = 17, product = 45, result = 10;
+// you can also declare and initialize more than one on one declaration
+
+// If you delare a variable without assigning it something, its default value is undefined, until you give 
+// the variable a value.
+
+var i;
+
+// variables can appear in for loops or functions as well:
+
+for(var i = 0; i < 10; i++) {
+    document.write(i , '<h1>')
+}
+
